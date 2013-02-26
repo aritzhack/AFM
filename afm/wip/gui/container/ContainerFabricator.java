1e138965735d264cae879f1b0e5f2780b3b53850
aritzhack <aritzhack@gmail.com>
2013-02-26 18:49:09 +0100

    More work on the fabricator, changed some slot IDs again, and cleaned some code

--- afm/wip/gui/container/ContainerFabricator.java
+++ afm/wip/gui/container/ContainerFabricator.java
@@ -1,5 +1,6 @@
 package afm.wip.gui.container;
 
+import afm.core.AFMLogger;
 import afm.core.util.UtilAFM;
 import afm.wip.tileEntity.BoundInvCrafting;
 import afm.wip.tileEntity.TEFabricator;
@@ -15,6 +16,7 @@ import net.minecraft.world.World;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * @author Aritz Lopez
@@ -47,10 +49,10 @@ public class ContainerFabricator extends Container {
 		// Result ID: 9
 		addSlotToContainer(new Slot(this.result, 9, 80, 35));
 
-		// Storage IDs: 10-18
+		// Storage IDs: 10-18 (10 get added in InvUpdater)
 		for (int y = 0; y < 3; y++) {
 			for (int x = 0; x < 3; x++) {
-				addSlotToContainer(new Slot(this.storage, 10 + x + y * 3, 116 + x * 18, 17 + y * 18));
+				addSlotToContainer(new Slot(this.storage, x + y * 3, 116 + x * 18, 17 + y * 18));
 			}
 		}
 
@@ -76,12 +78,8 @@ public class ContainerFabricator extends Container {
 	@Override
 	public ItemStack slotClick(int slotIndex, int button, int par3, EntityPlayer player) {
 		this.onCraftMatrixChanged(this.tileEntity);
-		if (this.fulfilled) {
-			System.out.println(this.tileEntity.worldObj.getWorldTime() + " - Fulfilled");
-		} else {
-			System.out.println(this.tileEntity.worldObj.getWorldTime() + " - Not Fulfilled");
-		}
-		System.out.println("---------------------------");
+		AFMLogger.log(this.tileEntity.worldObj.getWorldTime() + (this.fulfilled ? " - Fulfilled" : " - Not Fulfilled"));
+		AFMLogger.log("---------------------------" + par3);
 
 		if (slotIndex == 9) return null; // Can't modify result slot
 		if (slotIndex > 9 || slotIndex < 0) // If storage or none, handle them as usual
@@ -113,14 +111,14 @@ public class ContainerFabricator extends Container {
 
 		checkIfFulfilled();
 
-		super.onCraftMatrixChanged(par1IInventory);
+		//super.onCraftMatrixChanged(par1IInventory);
 	}
 
 	private void checkIfFulfilled() {
 
 		this.tempStorage.clear();
-
-		if (this.result.getStackInSlot(0) == null) return; // Not result in recipe -> Not check everything
+		ItemStack resultStack = this.result.getStackInSlot(0);
+		if (resultStack == null) return; // Not result in recipe -> Not check everything
 
 		// Clone the storage to a list, so that it can be modified
 		for (int i = 0; i < 9; i++) {
@@ -130,69 +128,65 @@ public class ContainerFabricator extends Container {
 		}
 
 		// Iterate over every itemStack needed
-		for (int i = 0; i < 9; i++) {
+		A: for (int i = 0; i < 9; i++) {
 			ItemStack is = this.matrix.getStackInSlot(i); // Get from the matrix
 			if (is == null) continue;
 			ItemStack neededStack = is.copy();
 
 			// Iterate over every itemStack in the storage
-			for (ItemStack curr : this.tempStorage) {
+			ListIterator<ItemStack> iter = this.tempStorage.listIterator();
+			if(!iter.hasNext()){
+				this.fulfilled = false;
+				return;
+			}
+			while (iter.hasNext()){
+				ItemStack curr = iter.next();
 				if (UtilAFM.isSameItemOreDict(curr, neededStack) && curr.stackSize >= 1) {
-					curr.splitStack(1); // 1, so that if in the recipe the stackSize is bigger, it doesn't affect
-					break;
+					ItemStack newIS = curr.copy();
+					newIS.splitStack(1); // 1, so that if in the recipe the stackSize is bigger, it doesn't affect
+					iter.remove();
+					if(newIS.stackSize >= 1){
+						iter.add(newIS);
+					}
+					continue A;
 				} else {
 					this.fulfilled = false;
 				}
 			}
 		}
 		if (this.fulfilled) {
-			int canFit = canFitInTempStorage(this.result.getStackInSlot(0));
-			if (canFit < 0) {
-				this.tempStorage.set(canFit, this.result.getStackInSlot(0));
-			} else if (canFit > 0) {
-				ItemStack is = this.result.getStackInSlot(0);
-				is.stackSize += this.tempStorage.get(-canFit).stackSize;
-				this.tempStorage.set(-canFit, is);
+			if(!addToTempStorage(resultStack)) return;
+			for(int i = 0; i < 9; i++){
+				try{
+					this.tileEntity.setInventorySlotContents(10+i, this.tempStorage.get(i));
+				} catch (IndexOutOfBoundsException e){
+					this.tileEntity.setInventorySlotContents(10+i, null);
+				}
 			}
 		}
 	}
 
-	/**
-	 * Returns the first slot number the itemStack can fit in,
-	 * returning a positive int if the slot is empty, and
-	 * a negative int if it's same item.
-	 *
-	 * @param itemStack ItemStack to be fit in the tempStorage
-	 * @return 0 if cannot fit
-	 *         >0 if slot num is null
-	 *         <0 if same item
-	 */
-	private int canFitInTempStorage(ItemStack itemStack) {
-		for (int i = 0; i < 9; i++) {
+	private boolean addToTempStorage(ItemStack itemStack) {
+		int canBeAdded = 0;
+		for (int i = 1; i < 10; i++) {
 			try {
 				ItemStack stack = this.tempStorage.get(i);
-
 				if (stack == null) {
-					return i;
-				} else if (stack.isItemEqual(itemStack) && itemStack.stackSize + stack.stackSize >= this.storage.getInventoryStackLimit()) {
-					return -i;
+					canBeAdded = i;
+					break;
+				} else if (stack.isItemEqual(itemStack) && itemStack.stackSize + stack.stackSize <= this.storage.getInventoryStackLimit()) {
+					canBeAdded = -i;
+					break;
 				}
-			} catch (IndexOutOfBoundsException e) {
-				continue;
-			}
+			} catch (IndexOutOfBoundsException ignored) {canBeAdded = 0;}
 		}
-		return 0;
-	}
-
-	// TODO check from the (to be done) cloned storage, so that we can then remove from it
-	private int findItemStackInStorage(ItemStack stack) {
-		for (int x = 0; x < 9; x++) {
-			ItemStack curr = this.tileEntity.getStackInSlot(x + 10);
-			if (UtilAFM.isSameItemOreDict(curr, stack)) {
-				return x;
-			}
+		if(canBeAdded == 0) return false;
+		if (canBeAdded > 0) {
+			this.tempStorage.set(canBeAdded-1, itemStack.copy());
+		} else if (canBeAdded < 0) {
+			itemStack.stackSize += this.tempStorage.get(-canBeAdded-1).stackSize;
 		}
-		return -1;
+		return true;
 	}
 
 	/**
@@ -200,7 +194,7 @@ public class ContainerFabricator extends Container {
 	 */
 	@Override
 	public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int par2) {
-		return super.transferStackInSlot(par1EntityPlayer, par2);    //To change body of overridden methods use File | Settings | File Templates.
+		return super.transferStackInSlot(par1EntityPlayer, par2);
 	}
 
 	public class InvUpdater implements IInventory {
@@ -218,24 +212,24 @@ public class ContainerFabricator extends Container {
 
 		@Override
 		public ItemStack getStackInSlot(int slot) {
-			return inv.getStackInSlot(slot);
+			return inv.getStackInSlot(slot+10);
 		}
 
 		@Override
 		public ItemStack decrStackSize(int slot, int amount) {
-			ItemStack ret = inv.decrStackSize(slot, amount);
+			ItemStack ret = inv.decrStackSize(slot+10, amount);
 			if (ret != null) ContainerFabricator.this.onCraftMatrixChanged(this);
 			return ret;
 		}
 
 		@Override
 		public ItemStack getStackInSlotOnClosing(int slot) {
-			return this.inv.getStackInSlotOnClosing(slot);
+			return this.inv.getStackInSlotOnClosing(slot+10);
 		}
 
 		@Override
 		public void setInventorySlotContents(int slot, ItemStack stack) {
-			this.inv.setInventorySlotContents(slot, stack);
+			this.inv.setInventorySlotContents(slot+10, stack);
 			ContainerFabricator.this.onCraftMatrixChanged(this);
 		}
 
