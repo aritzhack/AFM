a72d36347d1487b591d5ae2a54b32b04e8a3d51c
aritzhack <aritzhack@gmail.com>
2013-03-26 11:59:39 +0100

    Fabricator now works well with other fabs and double chests. Also reformatted

--- src/afm/wip/gui/container/ContainerFabricator.java
+++ src/afm/wip/gui/container/ContainerFabricator.java
@@ -9,6 +9,7 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
 import afm.core.AFMLogger;
@@ -21,7 +22,6 @@ import afm.wip.tileEntity.TEFabricator;
  * 
  * @author aritzh
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- * 
  */
 public class ContainerFabricator extends Container {
 	public TEFabricator tileEntity;
@@ -29,14 +29,13 @@ public class ContainerFabricator extends Container {
 	InventoryCraftResult result;
 
 	ItemStack[] tempStorage;
-	
+
 	IInventory[] neighbors = new IInventory[6];
 	ItemStack[][] neighArrays = new ItemStack[6][];
 
 	public boolean fulfilled;
 
-	public ContainerFabricator(TEFabricator teFabricator,
-			InventoryPlayer inventoryPlayer, World world) {
+	public ContainerFabricator(TEFabricator teFabricator, InventoryPlayer inventoryPlayer, World world) {
 		this.tileEntity = teFabricator;
 		this.matrix = new BoundInvCrafting(this);
 		this.result = new InventoryCraftResult();
@@ -48,33 +47,30 @@ public class ContainerFabricator extends Container {
 		// Crafting inventory IDs: 0-8
 		for (int y = 0; y < 3; y++) {
 			for (int x = 0; x < 3; x++) {
-				addSlotToContainer(new Slot(this.matrix, x + y * 3, 8 + x * 18,
-						17 + y * 18));
+				this.addSlotToContainer(new Slot(this.matrix, x + y * 3, 8 + x * 18, 17 + y * 18));
 			}
 		}
 
 		// Result ID: 9
-		addSlotToContainer(new Slot(this.result, 9, 80, 35));
+		this.addSlotToContainer(new Slot(this.result, 9, 80, 35));
 
 		// Storage IDs: 10-18
 		for (int y = 0; y < 3; y++) {
 			for (int x = 0; x < 3; x++) {
-				addSlotToContainer(new Slot(this.tileEntity, 10 + x + y * 3,
-						116 + x * 18, 17 + y * 18));
+				this.addSlotToContainer(new Slot(this.tileEntity, 10 + x + y * 3, 116 + x * 18, 17 + y * 18));
 			}
 		}
 
 		// Player Inventory
 		for (int y = 0; y < 3; y++) {
 			for (int x = 0; x < 9; x++) {
-				addSlotToContainer(new Slot(inventoryPlayer, 9 + x + y * 9,
-						8 + 18 * x, 84 + 18 * y));
+				this.addSlotToContainer(new Slot(inventoryPlayer, 9 + x + y * 9, 8 + 18 * x, 84 + 18 * y));
 			}
 		}
 
 		// Player hotbar
 		for (int x = 0; x < 9; x++) {
-			addSlotToContainer(new Slot(inventoryPlayer, x, 8 + 18 * x, 142));
+			this.addSlotToContainer(new Slot(inventoryPlayer, x, 8 + 18 * x, 142));
 		}
 
 	}
@@ -85,23 +81,17 @@ public class ContainerFabricator extends Container {
 	}
 
 	@Override
-	public ItemStack slotClick(int slotIndex, int button, int par3,
-			EntityPlayer player) {
+	public ItemStack slotClick(int slotIndex, int button, int shift, EntityPlayer player) {
 
-		if (slotIndex < 19 && slotIndex >= 0
-				&& this.getSlot(slotIndex).getHasStack()) {
-			AFMLogger.log("Slot stack id: "
-					+ this.getSlot(slotIndex).getStack().itemID + ", meta: "
-					+ this.getSlot(slotIndex).getStack().getItemDamage());
+		if (slotIndex < 19 && slotIndex >= 0 && this.getSlot(slotIndex).getHasStack()) {
+			AFMLogger.log("Slot stack id: " + this.getSlot(slotIndex).getStack().toString());
 		}
 
 		// Can't modify result slot
-		if (slotIndex == 9)
-			return null;
+		if (slotIndex == 9) return null;
 
 		// If storage or none, handle them as usual
-		if (slotIndex > 9 || slotIndex < 0)
-			return super.slotClick(slotIndex, button, par3, player);
+		if (slotIndex > 9 || slotIndex < 0) return super.slotClick(slotIndex, button, shift, player);
 
 		// Craft-matrix -> get stack in the cursor
 		ItemStack is = player.inventory.getItemStack();
@@ -129,8 +119,7 @@ public class ContainerFabricator extends Container {
 	@Override
 	public void onCraftMatrixChanged(IInventory inv) {
 
-		this.result.setInventorySlotContents(0, CraftingManager.getInstance()
-				.findMatchingRecipe(matrix, this.tileEntity.worldObj));
+		this.result.setInventorySlotContents(0, CraftingManager.getInstance().findMatchingRecipe(this.matrix, this.tileEntity.worldObj));
 
 		if (this.result.getStackInSlot(0) == null) {
 			this.fulfilled = true;
@@ -141,8 +130,8 @@ public class ContainerFabricator extends Container {
 	}
 
 	public void tryCraft() {
-		
-		updateArrays();
+
+		this.updateArrays();
 
 		ItemStack resultStack = this.result.getStackInSlot(0).copy();
 
@@ -160,33 +149,59 @@ public class ContainerFabricator extends Container {
 		}
 
 		// I have everything I need & can add stack to the storage7
-		if (addResult(resultStack)) {
+		if (this.addResult(resultStack)) {
 			this.updateInventories();
 		}
 	}
-	
-	private ItemStack[] invToISArray(IInventory inv, int startIndex, int length){
-		if(length <= 0 || length > inv.getSizeInventory()){ // Negative or zero length -> Whole inventory
+
+	private ItemStack[] invToISArray(IInventory inv, int startIndex, int length) {
+
+		TileEntityChest chest2 = null;
+
+		if ((length <= 0 || length > inv.getSizeInventory()) && !(inv instanceof TEFabricator)) { // Negative or zero length -> Whole inventory
 			length = inv.getSizeInventory();
 			startIndex = 0;
+		} else if (inv instanceof TEFabricator) {
+			length = 9;
+			startIndex = 10;
 		}
-		
+
 		ItemStack[] retArray = new ItemStack[length];
-		for (int i = startIndex; i < startIndex+length; i++) {
-			ItemStack itemStack = inv.getStackInSlot(i);
-			retArray[i-startIndex] = (itemStack == null ? null : itemStack.copy());
+
+		// If it's a chest, get the adjacent
+		if (inv instanceof TileEntityChest) {
+			TileEntityChest chest1 = (TileEntityChest) inv;
+			chest2 = chest1.adjacentChestXNeg != null ? chest1.adjacentChestXNeg : chest1.adjacentChestXPos != null ? chest1.adjacentChestXPos : chest1.adjacentChestZNeg != null ? chest1.adjacentChestZNeg : chest1.adjacentChestZPosition != null ? chest1.adjacentChestZPosition : null;
+			if (chest2 != null) {
+				retArray = new ItemStack[chest1.getSizeInventory() + chest2.getSizeInventory()];
+			}
 		}
+
+		for (int i = startIndex; i < startIndex + length; i++) {
+			ItemStack itemStack = inv.getStackInSlot(i);
+			retArray[i - startIndex] = itemStack == null ? null : itemStack.copy();
+		}
+		if (chest2 == null) return retArray;
+
+		// If there's a chest adjacent to the chest adjacent to the fabricator, add it to the array
+		for (int i = startIndex; i < startIndex + length; i++) {
+			ItemStack itemStack = chest2.getStackInSlot(i);
+			retArray[i - startIndex + inv.getSizeInventory()] = itemStack == null ? null : itemStack.copy();
+		}
+
 		return retArray;
 	}
-	
+
 	private void updateArrays() {
 		this.tempStorage = this.invToISArray(this.tileEntity, 10, 9);
-		
-		for(ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS){
-			IInventory inv = getInventoryInSide(dir);
-			if(inv == null) continue;
-			neighbors[dir.ordinal()] = inv;
-			neighArrays[dir.ordinal()] = invToISArray(inv, 0, 0);
+
+		for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+			IInventory inv = this.getInventoryInSide(dir);
+			if (inv == null) {
+				continue;
+			}
+			this.neighbors[dir.ordinal()] = inv;
+			this.neighArrays[dir.ordinal()] = this.invToISArray(inv, 0, 0);
 		}
 	}
 
@@ -195,53 +210,62 @@ public class ContainerFabricator extends Container {
 		int x = this.tileEntity.xCoord + dir.offsetX;
 		int y = this.tileEntity.yCoord + dir.offsetY;
 		int z = this.tileEntity.zCoord + dir.offsetZ;
-		
+
 		TileEntity te = w.getBlockTileEntity(x, y, z);
-		if(te instanceof IInventory) return (IInventory) te;
+		if (te instanceof IInventory) return (IInventory) te;
 		return null;
 	}
 
 	/**
 	 * Look for the IS in inventories
-	 * @param needed ItemStack to look for
+	 * 
+	 * @param needed
+	 *            ItemStack to look for
 	 * @return null if found, {@code needed} if not
 	 */
-	private ItemStack seekItem(ItemStack needed){
-		
-		needed = seekAndRemoveItemFromArray(needed, this.tempStorage);
-		if(needed == null) return null;
-		
-		for(ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS){
+	private ItemStack seekItem(ItemStack needed) {
+
+		needed = this.seekAndRemoveItemFromArray(needed, this.tempStorage);
+		if (needed == null) return null;
+
+		for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
 			IInventory inv = this.neighbors[dir.ordinal()];
-			if(inv == null) continue;
-			if(seekAndRemoveItemFromArray(needed, this.neighArrays[dir.ordinal()]) == null){
-				return null;
+			if (inv == null) {
+				continue;
 			}
+			if (this.seekAndRemoveItemFromArray(needed, this.neighArrays[dir.ordinal()]) == null) return null;
 		}
 		return needed;
 	}
-	
+
 	/**
 	 * Looks for an ItemStack in a IS[], and removes it from the array.
-	 * @param stack The IS to bee looked for
-	 * @param inv The array into which look for the IS
+	 * 
+	 * @param stack
+	 *            The IS to bee looked for
+	 * @param inv
+	 *            The array into which look for the IS
 	 * @return if found, null. Else return {@code stack}
 	 */
-	private ItemStack seekAndRemoveItemFromArray(ItemStack stack, ItemStack[] inv){
+	private ItemStack seekAndRemoveItemFromArray(ItemStack stack, ItemStack[] inv) {
 		for (int i = 0; i < inv.length; i++) {
 			ItemStack curr = inv[i];
-			if (curr == null) continue;
+			if (curr == null) {
+				continue;
+			}
 			// FIXME OreDict might lead to unwanted recipes. How to check??
 			if (UtilAFM.isSameItemOreDict(curr, stack) && curr.stackSize >= 1) {
 				curr.stackSize -= 1;
-				if (curr.stackSize < 1) inv[i] = null;
+				if (curr.stackSize < 1) {
+					inv[i] = null;
+				}
 				return null;
 			}
 		}
 		return stack;
 	}
-	
-	private boolean addResult(ItemStack stack){
+
+	private boolean addResult(ItemStack stack) {
 		int firstEmpty = -1;
 		try {
 			for (int i = 0; i < 9; i++) {
@@ -250,17 +274,15 @@ public class ContainerFabricator extends Container {
 					firstEmpty = i;
 				}
 				// FIXME ItemStack.isItemEqual doesn't handle NBT!
-				else if ((storageStack != null
-						&& storageStack.isItemEqual(stack) && stack.stackSize
-						+ storageStack.stackSize <= this.tileEntity
-							.getInventoryStackLimit())) {
+				else if (storageStack != null && storageStack.isItemEqual(stack) && stack.stackSize + storageStack.stackSize <= this.tileEntity.getInventoryStackLimit()) {
 					ItemStack newStack = storageStack.copy();
 					newStack.stackSize += stack.stackSize;
 					this.tempStorage[i] = newStack;
 					return true;
 				}
 			}
-		} catch (IndexOutOfBoundsException e) {}
+		} catch (IndexOutOfBoundsException e) {
+		}
 		if (firstEmpty != -1 && firstEmpty < 9) {
 			AFMLogger.log("Last option: " + firstEmpty);
 			this.tempStorage[firstEmpty] = stack;
@@ -270,7 +292,7 @@ public class ContainerFabricator extends Container {
 	}
 
 	private void updateInventories() {
-		
+
 		// Update internal storage
 		for (int i = 0; i < 9; i++) {
 			try {
@@ -279,33 +301,75 @@ public class ContainerFabricator extends Container {
 				this.tileEntity.setInventorySlotContents(i + 10, null);
 			}
 		}
-		
+
 		// Update neighbors' inventories
-		for(ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS){
-			
+		for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+
 			IInventory inv = this.neighbors[dir.ordinal()];
 			ItemStack[] tempInv = this.neighArrays[dir.ordinal()];
-			
-			if(inv == null) continue;
-			
-			for(int i = 0; i<inv.getSizeInventory();i++){
+
+			if (inv == null) {
+				continue;
+			}
+
+			int startIndex = 0;
+
+			if (inv instanceof TEFabricator) {
+				startIndex = 10; // If it's a fabricator, skip the crafting and result slots
+			}
+
+			for (int i = 0; i < inv.getSizeInventory(); i++) {
 				try {
-					inv.setInventorySlotContents(i, tempInv[i]);
+					inv.setInventorySlotContents(i + startIndex, tempInv[i]);
 				} catch (IndexOutOfBoundsException e) {
-					inv.setInventorySlotContents(i, null);
+					inv.setInventorySlotContents(i + startIndex, null);
+				}
+			}
+			if (inv instanceof TileEntityChest && tempInv.length > inv.getSizeInventory()) {
+				TileEntityChest chest1 = (TileEntityChest) inv;
+				TileEntityChest chest2 = null;
+				chest2 = chest1.adjacentChestXNeg != null ? chest1.adjacentChestXNeg : chest1.adjacentChestXPos != null ? chest1.adjacentChestXPos : chest1.adjacentChestZNeg != null ? chest1.adjacentChestZNeg : chest1.adjacentChestZPosition != null ? chest1.adjacentChestZPosition : null;
+				if (chest2 != null) {
+					for (int i = 0; i < +chest2.getSizeInventory(); i++) {
+						try {
+							chest2.setInventorySlotContents(i, tempInv[i + inv.getSizeInventory()]);
+						} catch (IndexOutOfBoundsException e) {
+							chest2.setInventorySlotContents(i, null);
+						}
+					}
 				}
 			}
 		}
 	}
 
 	/**
-	 * Called when putStackInSlot player shift-clicks on putStackInSlot slot.
-	 * You must override this or you will crash when someone does that.
+	 * Called when putStackInSlot player shift-clicks on putStackInSlot slot. You must override this or you will crash when someone does that.
 	 */
 	// TODO Make this work
 	@Override
-	public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int par2) {
-		return super.transferStackInSlot(par1EntityPlayer, par2);
+	public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int slotIndex) {
+
+		Slot slot = (Slot) this.inventorySlots.get(slotIndex);
+		AFMLogger.log("Slot " + slotIndex);
+
+		if (slot != null && slot.getHasStack()) {
+			ItemStack itemstack1 = slot.getStack();
+
+			if (slotIndex <= 9 && slotIndex > 19) // If storage
+			{
+				if (!this.mergeItemStack(itemstack1, 19, this.inventorySlots.size(), false)) return null;
+			} else if (slotIndex > 18) { // If player inventory
+				if (!this.mergeItemStack(itemstack1, 10, 9, true)) return null;
+			}
+
+			if (itemstack1.stackSize <= 0) {
+				slot.putStack((ItemStack) null);
+			} else {
+				slot.onSlotChanged();
+			}
+		}
+
+		return null;
 	}
 
 }
